8e3ca21de3edc07bf1a50e2a77c07598a3d98293
SplashyCoder <pachecomorahectordavid@gmail.com>
2026-02-04 13:10:49 -0500

    SQL Tutorial Lesson: Top-Selling Artists ready, using CTE

--- SQL/SQLTutorialLessonTop-SellingArtists.sql
+++ SQL/SQLTutorialLessonTop-SellingArtists.sql
@@ -1,4 +1,5 @@
-SELECT artist_name, 
+WITH ranks AS (SELECT 
+artist_name, 
 genre,
 concert_revenue, 
 number_of_members, 
@@ -8,3 +9,15 @@ RANK() OVER(
   ORDER BY concert_revenue/number_of_members DESC
 ) AS band_rank
 FROM concerts
+)
+
+SELECT
+artist_name, 
+genre,
+concert_revenue, 
+number_of_members,
+revenue_per_band_member
+FROM ranks
+WHERE band_rank = 1
+ORDER BY revenue_per_band_member DESC;
+
