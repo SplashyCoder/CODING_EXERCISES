5d8bedea231bb01bc40f27d0f34b9fb6cf218563
SplashyCoder <pachecomorahectordavid@gmail.com>
2026-02-06 13:35:12 -0500

    Supercloud Customer exercise ready

--- SQL/SupercloudCustomer.sql
+++ SQL/SupercloudCustomer.sql
@@ -1,14 +1,15 @@
 
-WIHT custormer_product_join AS (
+WITH custormer_product_join AS (
 SELECT 
-customer.customer_id, 
-products.product_id, 
-products.product_category 
+  customer.customer_id AS costomer_id, 
+  COUNT( DISTINCT products.product_category) AS product_category
 FROM customer_contracts AS customer 
 LEFT JOIN products AS products
-ON customer.product_id = products.product_id
-ORDER BY customer_id, product_id
+  ON customer.product_id = products.product_id
+GROUP BY customer.customer_id
+ORDER BY customer.customer_id
 )
 
+SELECT costomer_id FROM custormer_product_join
+ WHERE product_category >= 3
 ;
-
